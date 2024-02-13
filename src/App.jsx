089030caac9410f6eb8b1063abc2b684b9143cf3
= <dogukanbasarann@hotmail.com>
2024-02-13 17:38:50 +0300

    first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-6xl text-center'>
+      SOCIOLOGY QUESTION GAME FIRST TEMPLATE
+    </div>
+  )
+}
+
+export default App
